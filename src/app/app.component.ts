3741d0315f10bb6609b6cfb10034a18398037cbe
Viccenzo <viccenzobarbosa86@gmail.com>
2025-05-09 16:35:54 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   mostrarErro = false;
 
   constructor(private http: HttpClient) {
-    this.apiURL = 'https://mytodolist-end-production.up.railway.app';
+    this.apiURL = 'https://back-end-6ieu.onrender.com/';
     this.READ_tarefas();
   }
 
